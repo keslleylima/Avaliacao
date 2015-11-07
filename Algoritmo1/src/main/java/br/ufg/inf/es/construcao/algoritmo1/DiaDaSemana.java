e7f0856b494a0f1af78f0be615b7e06f60062bb0
keslleylima <keslley_14_20@hotmail.com>
2015-11-07 16:07:21 -0200

    Correção ortográfica

--- Algoritmo1/src/main/java/br/ufg/inf/es/construcao/algoritmo1/DiaDaSemana.java
+++ Algoritmo1/src/main/java/br/ufg/inf/es/construcao/algoritmo1/DiaDaSemana.java
@@ -1,20 +1,20 @@
 package br.ufg.inf.es.construcao.algoritmo1;
 
 /**
- * Implementação do algoritmo que obtem o dia da semana de uma data qualquer.
+ * Implementação do algoritmo que obtém o dia da semana de uma data qualquer.
  */
 public class DiaDaSemana {
 
     /**
      *
      * @param d dia da data;
-     * @param m mes da data;
+     * @param m Mês da data;
      * @param a ano da data;
      * @return valor s representa dia da semana daquela data;
      * s retorna 0 a 6 (0 segunda 1 terca 2-quarta ...);
-     * @throws IllegalArgumentException caso dia,mes ou ano invalido;
+     * @throws IllegalArgumentException caso dia,mês ou ano inválido;
      * O dia deve ser maior que 1 e menor que 31;
-     * o mes deve ser maior que 1 e menor que 12;
+     * o mês deve ser maior que 1 e menor que 12;
      * o ano deve ser maior que 1753;
      */
     public static int diaDaSemana(int d, int m, int a) {
@@ -23,7 +23,7 @@ public class DiaDaSemana {
         }
 
         if (m < 1 || m > 12) {
-            throw new IllegalArgumentException("mes invalido");
+            throw new IllegalArgumentException(" mês inválido");
         }
 
         if (a < 1753) {
