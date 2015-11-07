e4cd87030231d87dfae3220944d78f015b776a3a
keslleylima <keslley_14_20@hotmail.com>
2015-11-07 16:31:47 -0200

    Update Produto.java

--- Algoritmo4/src/main/java/br/ufg/inf/es/construcao/produto/Produto.java
+++ Algoritmo4/src/main/java/br/ufg/inf/es/construcao/produto/Produto.java
@@ -6,16 +6,16 @@ package br.ufg.inf.es.construcao.produto;
 public class Produto {
     /**
      *
-     * @param a numero qualquer que seja maior que 0;
-     * @param b numero igual ou maior do que 0;
+     * @param a n√∫mero qualquer que seja maior que 0;
+     * @param b n√∫mero igual ou maior do que 0;
      * @return s resultado do produto entre a e b;
      * throw new IllegalArgumentException caso a ou b
-     * nao satisfazerem a condiÁ„o;
+     * nao satisfazerem a condi√ß√£o;
      * variavel i funciona com contador dentro do loop;
      */
     public static int produto(int a, int b) {
         if (a < 0 ) {
-            throw new IllegalArgumentException("a n„o pode ser menor que 0");
+            throw new IllegalArgumentException("a n√£o pode ser menor que 0");
         }
 
         if (b < 0) {
