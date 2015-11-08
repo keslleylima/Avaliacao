715a1b2f43cf0032ba3a412f44b904a42c47fd74
keslleylima <keslley_14_20@hotmail.com>
2015-11-08 14:38:55 -0200

    Polinômio Simples

--- Algoritmo30/src/main/java/br/ufg/inf/es/construcao/polsimples/PolSimples.java
+++ Algoritmo30/src/main/java/br/ufg/inf/es/construcao/polsimples/PolSimples.java
@@ -1,7 +1,46 @@
 package br.ufg.inf.es.construcao.polsimples;
 
 /**
- * Created by keslleyLima on 08/11/2015.
+ * algoritmo que implementa o cálculo do polinomio simples;
  */
 public class PolSimples {
+    /**
+     *
+     * @param x número de elmentos inteiros;
+     * @param g inidce do vetor;
+     * @param a vetor que armazena os x números;
+     * @return p valor do polinomio;
+     * IllegalArgumentException caso não atenda as condicoes:
+     * tamanho do vetor >= indice do vetor + 1;
+     * todos elementos do vetor devem ser diferente de 0;
+     * o indice do vetor(g) deve ser igual ou maior que 1;
+     * varíavel de controle do loop: i;
+     * Uso da class potência para obter a potencia entre x ,g e x,i;
+     *
+     */
+    public static int polSimples(int x,int g,int[]a){
+    if (a.length < g+1){
+        throw new IllegalArgumentException("tamanho do vetor inválido");
+    }
+
+    if (a[g] == 0){
+        throw new IllegalArgumentException("vetor inválido");
+    }
+
+    if (g < 1){
+        throw new IllegalArgumentException("g inválido");
+    }
+
+    int t = Potencia.potencia(x,g);
+    int p = a[0];
+    int i = 1;
+
+    while (i <= g){
+        t = Potencia.potencia(x,i);
+        p = p + (a[i] * t);
+        i= i + 1;
+    }
+
+        return p;
+    }
 }
--- Algoritmo30/src/test/java/br/ufg/inf/es/construcao/polsimples/PolSimplesTest.java
+++ Algoritmo30/src/test/java/br/ufg/inf/es/construcao/polsimples/PolSimplesTest.java
@@ -1,16 +1,31 @@
 package br.ufg.inf.es.construcao.polsimples;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
-/**
- * Created by keslleyLima on 08/11/2015.
- */
 public class PolSimplesTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void ParametroGInvalido(){
+        int []vetor  = {1,2,3,4,5};
+        PolSimples.polSimples(0,1,vetor);
+    }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void ParametroTamanhoVetorInvalido(){
+        int []vetor  = {1,2,3,4,5};
+        PolSimples.polSimples(0,0,vetor);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void ParametroElementoVetorInvalido(){
+        int []vetor  = {1,2,3,4,5,0};
+        PolSimples.polSimples(2,6,vetor);
+    }
     @Test
     public void testPolSimples() throws Exception {
+        int []vetor = {1,2,3,4,5};
+        assertEquals(6,PolSimples.polSimples(1,2,vetor));
+        assertEquals(34,PolSimples.polSimples(3,2,vetor));
+        assertEquals(17,PolSimples.polSimples(2,2,vetor));
 
     }
 }
