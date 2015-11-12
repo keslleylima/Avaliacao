e841857610338596075cee26b4a39581941e5f42
keslleylima <keslley_14_20@hotmail.com>
2015-11-12 14:26:00 -0200

    Update Mod.java
    Correção da Documentação

--- Algoritmo27/src/main/java/br/ufg/inf/es/construcao/mod/Mod.java
+++ Algoritmo27/src/main/java/br/ufg/inf/es/construcao/mod/Mod.java
@@ -1,26 +1,27 @@
 package br.ufg.inf.es.construcao.mod;
 
 /**
- * implementaÁ„o do algoritmo que efetua x % y;
+ * implementa√ß√£o do algoritmo que efetua x % y;
  */
 public class Mod {
     /**
      *
      * @param x primeiro valor entrada,deve ser maior que 0;
      * @param y segundo valor entrada,deve ser maior ou igual a 0;
-     * @return s valor do resto da divis„o entre x e y;
-     * IllegalArgumentException caso x ou y n„o atenda as condiÁıes;
+     * @return s valor do resto da divis√£o entre x e y;
+     * @throwsllegalArgumentException caso x ou y n√£o atenda as condi√ß√µes;
      *
      */
     public static int mod(int x,int y){
         if (x <= 0){
-            throw new IllegalArgumentException("x inv·lido");
+            throw new IllegalArgumentException("x inv√°lido");
         }
 
         if (y < 0){
-            throw new IllegalArgumentException("y inv·lido");
+            throw new IllegalArgumentException("y inv√°lido");
         }
         int s = x;
+        
         while (s >= y){
             s = s - y;
         }
