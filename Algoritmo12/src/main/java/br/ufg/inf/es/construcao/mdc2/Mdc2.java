cffb815d97636a1cff27136fcb309067a3db7834
keslleylima <keslley_14_20@hotmail.com>
2015-11-12 14:06:59 -0200

    Correção da Documentação

--- Algoritmo12/src/main/java/br/ufg/inf/es/construcao/mdc2/Mdc2.java
+++ Algoritmo12/src/main/java/br/ufg/inf/es/construcao/mdc2/Mdc2.java
@@ -1,24 +1,24 @@
 package br.ufg.inf.es.construcao.mdc2;
 
 /**
- * algoritmo que implementa uma das possíveis formas de obter o mdc entre a e b;
+ * algoritmo que implementa uma das possÃ­veis formas de obter o mdc entre a e b;
  */
 public class Mdc2 {
     /**
      *
-     * @param a número/valor de entrada que deve maior ou igual a b;
-     * @param b número/valor de entrada que deve ser maior q zero;
+     * @param a nÃºmero/valor de entrada que deve maior ou igual a b;
+     * @param b nÃºmero/valor de entrada que deve ser maior q zero;
      * @return a valor do Mdc  entre a e b;
-     * IllegalArgumentException caso a ou b nao atenda as condições;
+     * @throws IllegalArgumentException  caso a ou b nao atenda as condiÃ§Ãµes;
      *
      */
     public static int mdc2(int a,int b) {
         if (a < b){
-           throw new IllegalArgumentException("a inválido ");
+           throw new IllegalArgumentException("a invÃ¡lido ");
         }
 
         if (b <= 0) {
-            throw new IllegalArgumentException("b inválido ");
+            throw new IllegalArgumentException("b invÃ¡lido ");
         }
 
         while (a != b){
