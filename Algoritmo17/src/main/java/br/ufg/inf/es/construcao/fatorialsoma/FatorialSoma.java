24a50e5fe6e3a866f71b7a394baa904fbfd5b5e2
keslleylima <keslley_14_20@hotmail.com>
2015-11-12 14:11:31 -0200

    Correção da Documentação

--- Algoritmo17/src/main/java/br/ufg/inf/es/construcao/fatorialsoma/FatorialSoma.java
+++ Algoritmo17/src/main/java/br/ufg/inf/es/construcao/fatorialsoma/FatorialSoma.java
@@ -1,7 +1,7 @@
 package br.ufg.inf.es.construcao.fatorialsoma;
 
 /**
- * implementação do algoritmo que calcula o fatorial de um numero por meio
+ * implementação do algoritmo que calcula o fatorial de um número por meio
  * de soma e produto apenas;
  */
 public class FatorialSoma {
@@ -9,13 +9,13 @@ public class FatorialSoma {
      *
      * @param n numero no qual deseja calcular seu fatorial;
      * @return f valor do fatorial do numero(n);
-     * IllegalArgumentException caso n seja menor que 1;
+     * @throws IllegalArgumentException  caso n seja menor que 1;
      * f = Produto.produto(f,i); fatorial(f) recebe o valor do produto entre
      * f e i que e calculado pela class chamada produto;
      */
     public static int fatorialSoma(int n){
         if (n < 1){
-            throw new IllegalArgumentException("n invalido");
+            throw new IllegalArgumentException("n inválido");
         }
 
         int i = 2;
