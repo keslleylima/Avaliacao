adee65d84fd8c663ffce38f6fdfb80945041e229
keslleylima <keslley_14_20@hotmail.com>
2015-11-12 14:46:05 -0200

    Update Hipotenusa.java
    Correção da Documentação

--- Algoritmo38/src/main/java/br/ufg/inf/es/construcao/hipotenusa/Hipotenusa.java
+++ Algoritmo38/src/main/java/br/ufg/inf/es/construcao/hipotenusa/Hipotenusa.java
@@ -1,29 +1,27 @@
 package br.ufg.inf.es.construcao.hipotenusa;
 
 /**
- * Implementação do algoritmo que efetua o  calcula da hipotenusa.
+ * ImplementaÃ§Ã£o do algoritmo que efetua o  calcula da hipotenusa.
  */
 public class Hipotenusa {
 
     /**
-     * Cálculo da hipotenusa;
+     * CÃ¡lculo da hipotenusa;
      *
      * @param A valor referente a pimeira medida.
      * @param B valor referente a segunda medida.
-     *
      * @return r, valor  da hipotenusa.
-     *
-     * IllegalArgumentException Caso :
+     * @throws llegalArgumentException Caso :
      * O valor de  A deve ser maior que 0.
      * O valor de  B deve ser maior que 0.
      */
     public static double hipotenusa(int a, int b) {
         if (a <= 0) {
-            throw new IllegalArgumentException("a inválido");
+            throw new IllegalArgumentException("a invÃ¡lido");
         }
 
         if (b <= 0) {
-            throw new IllegalArgumentException("b inválido");
+            throw new IllegalArgumentException("b invÃ¡lido");
         }
 
         int x = a * a;
