56ec9da781a8be8d24a2654d303d2cac7d976aac
keslleylima <keslley_14_20@hotmail.com>
2015-11-12 14:33:33 -0200

    Update PolHorner.java
    Correção da Documentação

--- Algoritmo31/src/main/java/br/ufg/inf/es/construcao/polhorner/PolHorner.java
+++ Algoritmo31/src/main/java/br/ufg/inf/es/construcao/polhorner/PolHorner.java
@@ -1,25 +1,25 @@
 package br.ufg.inf.es.construcao.polhorner;
 
-/** algoritmo que implementa o esquema de horner para avaliação dos poliômios;
+/** algoritmo que implementa o esquema de horner para avaliaÃ§Ã£o dos poliÃ´mios;
  */
 public class PolHorner {
     /**
      *
      * @param a vetor;
-     * @param g índice do vetor a;
-     * @param x número de elementos a ser verificado;
-     * @return valor do polinômio;
-     * IllegalArgumentException caso g seja menor que ou tamanho do vetor seja
+     * @param g Ã­ndice do vetor a;
+     * @param x nÃºmero de elementos a ser verificado;
+     * @return valor do polinÃ´mio;
+     * @throws IllegalArgumentException caso g seja menor que ou tamanho do vetor seja
      * menor que g + 1;
      *
      */
     public static int polHorner(int [] a, int g, int x){
         if (g < 1){
-            throw new IllegalArgumentException("G Inválido");
+            throw new IllegalArgumentException("G InvÃ¡lido");
         }
 
         if (a.length <g+1){
-            throw new IllegalArgumentException("Tamanho do Vetor Inválido");
+            throw new IllegalArgumentException("Tamanho do Vetor InvÃ¡lido");
         }
 
         int p = a[g];
